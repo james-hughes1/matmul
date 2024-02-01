500cf3aef2d2f25cdcacf5f8c45bcbc7084a023a
James Hughes <jh2284@cam.ac.uk>
2024-02-01 21:15:08 +0000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    double x,y,z;
+
+    x = 20e0;
+    y = 5e0;
+
+    z = x + y;
+
+    std::cout << "Output:" << x << " + " << y << " = " << z << std::endl;
+
+    return 0;
+}
