d9d054178405eab19e5d079a596f2e9a147b1967
James Hughes <jh2284@cam.ac.uk>
2024-02-05 14:21:41 +0000

    Build: Restructure source code and builds with multiply.cpp file.

---
+++ src/multiply/include/multiply.h
@@ -0,0 +1,16 @@
+#include <string>
+
+#ifndef MULTIPLY_H
+#define MULTIPLY_H
+
+/**
+ * @brief Library for matrix multiplication functions
+ * contained in the namespace "multiply"
+ *
+ */
+namespace multiply
+{
+int matmul(std::string filename);
+} // namespace multiply
+
+#endif // MULTIPLY INCLUDE
---
+++ src/multiply/multiply.cpp
@@ -0,0 +1,68 @@
+/**
+ * \file multiply.cpp Contains useful functions for multiplying matrices.
+ */
+
+#include "include/multiply.h"
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int multiply::matmul(std::string filename)
+{
+    /**
+     * \brief Multiplies two matrices in a given .txt file
+     * \param filename Path of the relevant input file.
+     */
+    std::ifstream input_file;
+    input_file.open(filename);
+    std::vector<char> file_chars(0);
+    char nextchar;
+    while (!input_file.eof())
+    {
+        nextchar = input_file.get();
+        file_chars.push_back(nextchar);
+    }
+    input_file.close();
+
+    std::vector<double> input_floats(0);
+    std::string nextstr;
+    for (int i = 0; i < file_chars.size(); i++)
+    {
+        nextchar = file_chars[i];
+        if (isdigit(nextchar) or nextchar == '.' or nextchar == 'e' or
+            nextchar == '-')
+        {
+            nextstr += nextchar;
+        }
+        else
+        {
+            if (nextstr.length() > 0)
+            {
+                input_floats.push_back(std::stod(nextstr));
+                nextstr.clear();
+            }
+        }
+    }
+    const int p = static_cast<const int>(input_floats[0]);
+    const int q = static_cast<const int>(input_floats[1]);
+    const int r = static_cast<const int>(input_floats[2 + p * q + 1]);
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < r; j++)
+        {
+            double z = 0;
+            for (int k = 0; k < q; k++)
+            {
+                double Aik = input_floats[2 + i * q + k];
+                double Bkj = input_floats[2 + p * q + 2 + k * r + j];
+                z += Aik * Bkj;
+            }
+            std::cout << z << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
